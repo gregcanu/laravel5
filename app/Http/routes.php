863dfc27fab1e6ea5a42b03d96b8fd1ac13f6657
Gregoire <canugregoire@gmail.com>
2020-02-06 17:28:41 +0100

    Contrôleurs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,22 +11,18 @@
   |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
-Route::get('{n}', function($n) {
-    return response('Je suis la page ' . $n . ' !', 200);
-})->where('n', '[1-3]');
+Route::get('article/{n}', 'ArticleController@show')->where('n', '[0-9]+');
 
-Route::get('/', function() {
-    return view('vue1');
-});
-
-Route::get('article/{n}', function($n) {
-    return view('article')->with('numero', $n);
-})->where('n', '[0-9]+');
-
-Route::get('facture/{n}', function($n) { 
-    return view('facture')->withNumero($n); 
-})->where('n', '[0-9]+');
+//Route::get('{n}', function($n) {
+//    return response('Je suis la page ' . $n . ' !', 200);
+//})->where('n', '[1-3]');
+//
+//Route::get('article/{n}', function($n) {
+//    return view('article')->with('numero', $n);
+//})->where('n', '[0-9]+');
+//
+//Route::get('facture/{n}', function($n) { 
+//    return view('facture')->withNumero($n); 
+//})->where('n', '[0-9]+');
