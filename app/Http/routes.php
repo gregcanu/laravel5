68768cae63dc457fdd6fdc00778b0ff59019286d
Gregoire <canugregoire@gmail.com>
2020-02-06 15:14:14 +0100

    Routes et réponses

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,32 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{n}', function($n) {
+    return response('Je suis la page ' . $n . ' !', 200);
+})->where('n', '[1-3]');
+
+Route::get('/', function() {
+    return view('vue1');
+});
+
+Route::get('article/{n}', function($n) {
+    return view('article')->with('numero', $n);
+})->where('n', '[0-9]+');
+
+Route::get('facture/{n}', function($n) { 
+    return view('facture')->withNumero($n); 
+})->where('n', '[0-9]+');
